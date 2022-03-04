5305fb3497dc2ee547058fe2c7b7acaab0cb6833
TumeFlizot2021 <TumemichevFlizot1002@gmail.com>
2022-03-04 14:04:02 +0100

    actually swapped the values

--- hello.c
+++ hello.c
@@ -9,6 +9,10 @@ int main()
   printf("Value of a: %d", a);
   printf("Value of b: %d", b);
   
+  int temp = a;
+  a = b;
+  b = temp; 
+  
   printf("After swapping");
   printf("Value of a: %d", a);
   printf("Value of b: %d", b);
