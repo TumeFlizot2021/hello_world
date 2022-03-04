921ea3a4954d208bf4cf29742d4765a61ab4fd3e
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-04 13:54:27 +0100

    pulled hello.c to github

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 45;
+  int b = 60;
+  
+  printf("Before swapping");
+  printf("Value of a: %d", a);
+  printf("Value of b: %d", b);
+  
+  printf("After swapping");
+  printf("Value of a: %d", a);
+  printf("Value of b: %d", b);
+  
+}
